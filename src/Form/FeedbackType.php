3a38a9a189070aec07450161d513b804b17c1eeb
Lesha Lebedev <lesha744@gmail.com>
2021-04-12 18:00:24 +0300

    123

--- src/Form/FeedbackType.php
+++ src/Form/FeedbackType.php
@@ -6,6 +6,7 @@ use App\Entity\Feedback;
 use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type;
 class FeedbackType extends AbstractType
@@ -18,7 +19,7 @@ class FeedbackType extends AbstractType
             ->add('phone', Type\TelType::class, ['label'=>false, 'attr'=>['class' => 'modal-form__input input js-contact-group js-phone', 'placeholder' => 'Телефон' , 'required' => false ,]])
             ->add('message',Type\TextareaType::class, ['label'=>false, 'attr'=>['class' => 'modal-form__textarea textarea', 'placeholder' => 'Коментарий', ]] )
             ->add('save', Type\SubmitType::class, [
-
+                'attr'=>['class' => 'modal-form__btn btn btn--type-1']
             ])
         ;
     }
