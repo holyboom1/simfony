a36150cafbe4dda2357d9864fae29438d461c371
iholy <lesha744@gmail.com>
2021-04-21 18:03:44 +0300

    mailer

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -7,30 +7,47 @@ namespace App\Controller;
 use App\Entity\Config;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MailerController extends AbstractController
 {
-    /**
-     * @Route("/email")
-     */
-    public function sendEmail(MailerInterface $mailer)
+
+    public $mail;
+
+//    /**
+//     * @Route("/email")
+//     */
+    public function sendEmail(MailerInterface $mailer )
     {
-        $em = $this->getDoctrine()->getRepository(Config::class)->findOneBy(['lang'=>'ru'])->getAdminEmail();
-        $email = (new TemplatedEmail())
-            ->from($em)
-            ->to('you@example.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->htmlTemplate('mail/email.html.twig');
+
+//        $em = $this->getDoctrine()->getRepository(Config::class)->findOneBy(['lang'=>'ru'])->getAdminEmail();
+//        $email = (new TemplatedEmail())
+//            ->from($em)
+//            ->to('you@example.com')
+//            //->cc('cc@example.com')
+//            //->bcc('bcc@example.com')
+//            //->replyTo('fabien@example.com')
+//            //->priority(Email::PRIORITY_HIGH)
+//            ->subject('Time for Symfony Mailer!')
+//            ->text('Sending emails is fun again!')
+//            ->htmlTemplate('mail/email.html.twig');
+//
+//        $mailer->send($email);
+
+        print_r($this->mail);
+        $email = (new Email())
+            ->from('weqwe@qweqw.com')
+            ->to('asdads@asd.com')
+            ->subject('asdasd')
+            ->html('<p>asdasd</p>');
 
         $mailer->send($email);
+
+
+        return new Response("email send");
 
         // ...
     }
